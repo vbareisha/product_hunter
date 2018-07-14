c7a3097dca0a57cca08d41ab9f6ca34396bd2491
vbareisha <vbareisha@users.noreply.github.com>
2018-07-14 16:04:53 +0300

    - deleted comment;

--- src/main/java/com/bareisha/producthunter/ProductHunterApplication.java
+++ src/main/java/com/bareisha/producthunter/ProductHunterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductHunterApplication {
 
 	public static void main(String[] args) {
-		//start
+
 		SpringApplication.run(ProductHunterApplication.class, args);
 	}
 }
