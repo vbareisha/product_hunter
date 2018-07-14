c49b5911e0c9d207a802dc939ecf2ef4e1db8994
vbareisha <vbareisha@users.noreply.github.com>
2018-07-14 15:27:46 +0300

    - initial commit;

---
+++ src/main/java/com/bareisha/producthunter/ProductHunterApplication.java
@@ -0,0 +1,12 @@
+package com.bareisha.producthunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductHunterApplication.class, args);
+	}
+}
