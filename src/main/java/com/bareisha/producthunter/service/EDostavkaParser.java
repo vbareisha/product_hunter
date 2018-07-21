6dca4893692c3d231fae29dd902f23a0002b709f
vbareisha <vbareisha@users.noreply.github.com>
2018-07-21 19:06:23 +0300

    - Getting info from page (title, country, price);

--- src/main/java/com/bareisha/producthunter/service/EDostavkaParser.java
+++ src/main/java/com/bareisha/producthunter/service/EDostavkaParser.java
@@ -5,6 +5,8 @@ import com.bareisha.producthunter.service.api.IParserHtml;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -17,7 +19,7 @@ public class EDostavkaParser implements IParserHtml {
     public boolean parse(String path) {
         try {
             Document document = Jsoup.connect(path).get();
-            System.out.println(document.body());
+            getDocumentData(document);
         } catch (IOException e) {
             log.error("Error in getting data from path = {}", path);
             log.error("Error: {}", e.getMessage());
@@ -31,12 +33,29 @@ public class EDostavkaParser implements IParserHtml {
         try {
             File file = new File(path);
             Document document = Jsoup.parse(file, "UTF-8");
-            System.out.println(document.body());
+            getDocumentData(document);
         } catch (IOException e) {
             log.error("Error in getting data from path = {}", path);
             log.error("Error: {}", e.getMessage());
             throw new PageByUrlNotFoundException(String.format("Page by path = %s not found!", path));
         }
         return false;
+    }
+
+    private void getDocumentData(Document document) {
+        Elements elementFormList = document.select("div.form_wrapper");
+        for (Element element : elementFormList) {
+            //title
+            System.out.println(element.select("div.title").select("a[href]").text());
+            //country
+            System.out.println(element.select("div.small_country").text());
+            //price
+            System.out.println(element.select("div.prices__wrapper")
+                    .select("div.services_wrap")
+                    .select("div.prices_block")
+                    .select("div.price_byn")
+                    .select("div.price").text());
+
+        }
     }
 }
