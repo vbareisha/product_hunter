b0648dd7276cabde938172dc44e6836a63783f68
vbareisha <vbareisha@users.noreply.github.com>
2018-07-29 19:12:44 +0300

    - added travis settings for database;

--- src/test/java/com/bareisha/producthunter/repository/ProductRepositoryTest.java
+++ src/test/java/com/bareisha/producthunter/repository/ProductRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
