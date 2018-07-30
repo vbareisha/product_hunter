f7c99966f889be127f781a2ebc3c56e479b9e241
vbareisha <vbareisha@users.noreply.github.com>
2018-07-30 20:21:55 +0300

    - added new controller;

---
+++ src/main/java/com/bareisha/producthunter/web/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.bareisha.producthunter.web.controller;
+
+import com.bareisha.producthunter.core.dto.ProductDto;
+import com.bareisha.producthunter.service.api.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final IProductService<ProductDto> productService;
+
+    @Autowired
+    public ProductController(IProductService<ProductDto> productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllProducts() {
+        return new ResponseEntity<>(productService.findAllProducts(), OK);
+    }
+}
