48bc5bdbca86cd255e0ea543bab58342cd347d36
vbareisha <vbareisha@users.noreply.github.com>
2019-10-30 15:07:14 +0300

    - refactor tests for HealthStatusController.java;

--- src/test/java/com/bareisha/producthunter/web/controller/HealthStatusControllerTest.java
+++ src/test/java/com/bareisha/producthunter/web/controller/HealthStatusControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,39 +29,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 public class HealthStatusControllerTest {
 
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(),
-			Charset.forName("utf8"));
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            StandardCharsets.UTF_8);
 
-	private void setup() {
-		MockitoAnnotations.initMocks(this);
-		this.mockMvc = MockMvcBuilders
-				.standaloneSetup(healthStatusController)
-				.build();
-	}
+    private void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(healthStatusController)
+                .build();
+    }
 
-	@InjectMocks
-	private HealthStatusController healthStatusController;
+    @InjectMocks
+    private HealthStatusController healthStatusController;
 
-	/**
-	 * Test for health controller
-	 * @throws Exception
-	 */
-	@Test
-	public void checkStatus() throws Exception {
-		setup();
-		HttpHeaders headers = new HttpHeaders();
+    /**
+     * Test for health controller
+     */
+    @Test
+    public void checkStatus() throws Exception {
+        setup();
+        HttpHeaders headers = new HttpHeaders();
 
-		MvcResult mvcResult = this.mockMvc
-				.perform(get("/status").contentType(contentType).headers(headers))
-				.andExpect(status().isOk()).andReturn();
-		Map response = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Map.class);
-		Assert.assertTrue(response.containsKey("status"));
-		Assert.assertTrue(response.containsValue("ok"));
-	}
+        MvcResult mvcResult = this.mockMvc
+                .perform(get("/status").contentType(contentType).headers(headers))
+                .andExpect(status().isOk()).andReturn();
+        Map response = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Map.class);
+        Assert.assertTrue(response.containsKey("status"));
+        Assert.assertTrue(response.containsValue("ok"));
+    }
 }
--- src/test/java/com/bareisha/producthunter/web/controller/HealthStatusTestConfiguration.java
+++ src/test/java/com/bareisha/producthunter/web/controller/HealthStatusTestConfiguration.java
@@ -1,18 +1,13 @@
 package com.bareisha.producthunter.web.controller;
 
-import com.bareisha.producthunter.core.dto.ProductDto;
-import com.bareisha.producthunter.service.api.IParserHtml;
-import com.bareisha.producthunter.service.api.IProductService;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @TestConfiguration
-@ComponentScan(basePackages = "com.bareisha.producthunter.web.controller")
+@ComponentScan(
+        basePackages = "com.bareisha.producthunter.web.controller",
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE, classes = {ParserController.class, ProductController.class}))
 public class HealthStatusTestConfiguration {
-    @MockBean
-    private IParserHtml parserHtml;
-
-    @MockBean
-    private IProductService<ProductDto> productService;
 }
